1abf6bb8f4b0107b8348125a796fac2437c05578
Eyal Kalderon <ebkalderon@gmail.com>
2018-08-31 19:34:19 +0800

    Add version specifier in build.rs

--- build.rs
+++ build.rs
@@ -4,6 +4,7 @@ extern crate freedesktop_categories_codegen as codegen;
 fn main() {
     #[cfg(feature = "generate-map")]
     codegen::DesktopMenuSpec::new()
+        .version(codegen::Version::Latest)
         .output_file_name("map.rs")
         .gen_static_map()
         .unwrap();
